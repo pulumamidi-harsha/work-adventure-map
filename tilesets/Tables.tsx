04dcb29b7ea495db5a3df9fdfeab956229692bd6
harishbabu-sriram <harishbabu.sriram.ext@bayer.com>
2025-12-01 15:27:53 +0530

    initial commit

---
+++ tilesets/Tables.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Tables" tilewidth="32" tileheight="32" tilecount="270" columns="10">
+ <image source="Tables.png" width="320" height="864"/>
+</tileset>
---
+++ tilesets/map_zones.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="map_zones" tilewidth="32" tileheight="32" tilecount="12" columns="6">
+ <image source="Special_Zones.png" width="192" height="64"/>
+ <tile id="2">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
